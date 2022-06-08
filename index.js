591c00bbea6eb1524988190ab622cc68d72ca068
backs <26674182+Backss4@users.noreply.github.com>
2022-06-08 11:30:08 +0200

    Małe poprawki

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ passport.use(new GoogleStrategy({
       pool.query('SELECT * FROM users WHERE external_id = $1 AND provider = \'google\'', [profile.sub])
       .then((res) => {
         const user = res.rows[0]
+        console.log(user)
         pool.query("UPDATE users SET lastvisit=CURRENT_TIMESTAMP(), counter = $1 WHERE id = $2", [user.counter, user.id]).then(() => {
           done(null, user);
         }).catch((err) => {
